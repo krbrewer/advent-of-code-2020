ba992ffb8d5388120b5a7d5617fbf773c51b21ab
Kevin Brewer <kevinbrewer@kevins-mbp.lan>
2020-12-03 18:43:05 -0600

    cleanup day 1 code

--- day1/part1.rb
+++ day1/part1.rb
@@ -1,15 +1,15 @@
-$file_data = File.read("day1.txt").split
+file_data = File.read("day1.txt").split
 
-$i = 0
+i = 0
 
-while $i < $file_data.length do
-    $j = $i
-    $diff = 2020 - $file_data[$i].to_i
-    while $j < $file_data.length do
-        if $diff === $file_data[$j].to_i
-            puts $file_data[$i].to_i * $file_data[$j].to_i
+while i < file_data.length do
+    j = i
+    diff = 2020 - file_data[i].to_i
+    while j < file_data.length do
+        if diff === file_data[j].to_i
+            puts file_data[i].to_i * file_data[j].to_i
         end
-        $j +=1
+        j +=1
     end
-    $i +=1
+    i +=1
 end
--- day1/part2.rb
+++ day1/part2.rb
@@ -1,19 +1,19 @@
-$file_data = File.read("day1.txt").split
+file_data = File.read("day1.txt").split
 
-$i = 0
+i = 0
 
-while $i < $file_data.length do
-    $j = $i
-    while $j < $file_data.length do
-        $k = $j
-        $diff = 2020 - $file_data[$i].to_i - $file_data[$j].to_i
-        while $k < $file_data.length do
-            if $diff === $file_data[$k].to_i
-                puts $file_data[$i].to_i * $file_data[$j].to_i * $file_data[$k].to_i
+while i < file_data.length do
+    j = i
+    while j < file_data.length do
+        k = j
+        diff = 2020 - file_data[i].to_i - file_data[j].to_i
+        while k < file_data.length do
+            if diff === file_data[k].to_i
+                puts file_data[i].to_i * file_data[j].to_i * file_data[k].to_i
             end
-            $k +=1
+            k +=1
         end
-        $j +=1
+        j +=1
     end
-    $i +=1
+    i +=1
 end
